89333cad93d5af1181631bd12c1d981b7c4e908d
Damian Legawiec <damian@sparksolutions.co>
2015-11-01 13:59:59 +0100

    proper development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.paperclip_defaults = {
+    url: '/:class/:attachment/:id_partition/:style/:filename',
+    path: ':rails_root/public/:class/:attachment/:id_partition/:style/:filename',
+  }
 end
