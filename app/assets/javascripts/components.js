aaa9a0046d10a8fea9285e614c5685952cad18fb
Damian Legawiec <damian@sparksolutions.co>
2015-12-05 21:22:34 +0100

    old dummy components removed

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -6,9 +6,6 @@ var ReactDOMServer = global.ReactDOMServer = require('react-dom/server');
 
 var App = window.App = global.App = {};
 
-// dumb components exposed for react-rails
-App.DemoComponent = require('./components/demo_component').default;
-
 // redux providers exposed for react-rails
 App.CartProvider = require('./providers/cart_provider').default;
 App.AccountProvider = require('./providers/account_provider').default;
