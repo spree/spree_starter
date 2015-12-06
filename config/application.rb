fd17f75296d21f30e5adbab4c012d0c6834df042
Damian Legawiec <damian@sparksolutions.co>
2015-12-06 20:01:18 +0100

    naming change

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SparkSpreeBoilerplate
+module SparkStarterKit
   class Application < Rails::Application
 
     config.to_prepare do
