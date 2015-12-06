c393299dd3eefba3276d4e48105be6a8e6e8f285
Damian Legawiec <damian@sparksolutions.co>
2015-12-06 11:35:05 +0100

    new simplified layout (WIP)

--- app/assets/javascripts/components/cart.js
+++ app/assets/javascripts/components/cart.js
@@ -3,14 +3,11 @@ import React, { Component, PropTypes } from 'react'
 export default class Cart extends Component {
   render() {
     return (
-      <ul className="nav navbar-nav navbar-right">
-        <li id="link-to-cart">
-          <a className="cart-info empty" href={Routes.spree_cart_path()}>
-            <span className="glyphicon glyphicon-shopping-cart"></span>
-            Cart: {this.props.total}
-          </a>
-        </li>
-      </ul>
+      <a href={Routes.spree_cart_path()}>
+        <span className="glyphicon glyphicon-shopping-cart" />
+        &nbsp;
+        Cart: {this.props.total}
+      </a>
     )
   }
 }
