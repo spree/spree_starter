b79018a0969b9dd43e4a4bc3e66afe0d9999197f
Damian Legawiec <damian@sparksolutions.co>
2015-12-08 11:41:16 +0100

    redux dev tools fixes

--- app/assets/javascripts/store/index.js
+++ app/assets/javascripts/store/index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   module.exports = require('./index.prod');
 } else {
   module.exports = require('./index.dev');
--- app/assets/javascripts/store/index.prod.js
+++ app/assets/javascripts/store/index.prod.js
@@ -1,7 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
-
-const loggerMiddleware = createLogger()
 
 const finalCreateStore = compose(
   applyMiddleware(thunkMiddleware)
