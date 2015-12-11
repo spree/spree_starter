d36df2f25495743165406a8cf53d965df5bf5ff4
Damian Legawiec <damian@sparksolutions.co>
2015-12-11 15:12:47 +0100

    turn on cloudflare fix only on production

--- config/application.rb
+++ config/application.rb
@@ -66,8 +66,10 @@ module SparkStarterKit
     # use LibSass for Sass compilcation
     config.css_compressor = :sassc
 
-    # CloudFlare middleware for proper visitors IP addresses
-    require "#{Rails.root}/lib/cloud_flare_middleware"
-    config.middleware.insert_before(0, Rack::CloudFlareMiddleware)
+    if Rails.env.production?
+      # CloudFlare middleware for proper visitors IP addresses
+      require "#{Rails.root}/lib/cloud_flare_middleware"
+      config.middleware.insert_before(0, Rack::CloudFlareMiddleware)
+    end
   end
 end
