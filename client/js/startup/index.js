b3d5864ebac8887eb401158bc3a5b703897ee963
Damian Legawiec <damian@sparksolutions.co>
2016-01-01 16:24:36 +0100

    unnecessary file removed

--- client/js/startup/index.js
+++
@@ -1,7 +0,0 @@
-import CartProvider from 'providers/CartProvider'
-import AccountProvider from 'providers/AccountProvider'
-import Dummy from 'components/Dummy'
-
-window.CartProvider = CartProvider
-window.AccountProvider = AccountProvider
-window.Dummy = Dummy
