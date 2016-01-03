e8faa16b23181e9c6822207727f12c7b2aecc51d
Damian Legawiec <damian@sparksolutions.co>
2016-01-03 20:01:28 +0100

    production store fix

--- client/js/store/index.prod.js
+++ client/js/store/index.prod.js
@@ -2,10 +2,15 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from 'reducers'
 
-export default props => {
+const finalCreateStore = (props) => {
   const initialState = props
   const composedStore = compose(
     applyMiddleware(thunkMiddleware)
   )
   return composedStore(createStore)(rootReducer, initialState)
 }
+
+export default finalCreateStore
+
+// for use with multiple providers in classic rails views
+export const createdStore = finalCreateStore()
