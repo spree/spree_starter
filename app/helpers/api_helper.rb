e180088ec06f5a8c627d091c8cf80ce6ff6485b0
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2016-03-05 14:24:50 +0100

    set spree_current_user as serialization scope for AMS

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -8,7 +8,7 @@ module ApiHelper
   end
 
   def json_serialized(object = {}, options = {})
-    options.reverse_merge!(include: '**')
+    options.reverse_merge!(include: '**', scope: spree_current_user, scope_name: :spree_current_user)
     ActiveModel::SerializableResource.new(object, options).serializable_hash
   end
 
