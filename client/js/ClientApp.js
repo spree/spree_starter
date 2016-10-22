b7e102a00881bdc2ee51740cb40c6d2ac42d48bc
Matt <niewskici@gmail.com>
2016-10-22 13:40:45 +0200

    Chrome addon devtools are superior, so if present it should be used

--- client/js/ClientApp.js
+++ client/js/ClientApp.js
@@ -22,7 +22,7 @@ export default (props) => {
           <Router history={history}>
             {routes}
           </Router>
-          <DevTools />
+          {!window.devToolsExtension && <DevTools/>}
         </div>
       </Provider>
     )
