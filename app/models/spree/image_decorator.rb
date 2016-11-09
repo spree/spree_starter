3202f679aa6138df6af315c83ce89327d73f5a81
symarys <maria.syczewska@sparksolutions.co>
2016-11-09 19:16:14 +0100

    Increase density for product image styles

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -3,19 +3,19 @@ Spree::Image.class_eval do
                     styles: {
                       mini: {
                         geometry: '48x48>',
-                        source_file_options: '-density 500 -quality 100'
+                        source_file_options: '-density 1200 -quality 100'
                       },
                       small: {
                         geometry: '100x100>',
-                        source_file_options: '-density 500 -quality 100'
+                        source_file_options: '-density 1200 -quality 100'
                       },
                       product: {
                         geometry: '240x240>',
-                        source_file_options: '-density 500 -quality 100'
+                        source_file_options: '-density 1200 -quality 100'
                       },
                       large: {
                         geometry: '600x600>',
-                        source_file_options: '-density 500 -quality 100'
+                        source_file_options: '-density 1200 -quality 100'
                       }
                     },
                     default_style: :product,
