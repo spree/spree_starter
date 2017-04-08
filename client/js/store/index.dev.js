13d0b5145e9ef6b66e99c3f9255d6045551d15a7
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2017-04-08 13:27:59 +0200

    update redux-logger (^3.0.1)

--- client/js/store/index.dev.js
+++ client/js/store/index.dev.js
@@ -1,6 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import rootReducer from 'reducers'
 import DevTools from 'containers/DevTools'
 
