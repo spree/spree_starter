20f962b9e5ed991a30ee99d1c2dbd2ec36325dac
Janek <jang.lachowicz@gmail.com>
2017-04-12 11:13:55 +0200

    Add custom controller for Gaffe errors

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,14 @@
+class ErrorsController < Spree::BaseController
+  include Gaffe::Errors
+
+  before_action :set_request_params, only: :show
+
+  layout 'application_classic'
+
+  private
+
+  def set_request_params
+    request.params['controller'] = controller_name
+    request.params['action'] = action_name
+  end
+end
