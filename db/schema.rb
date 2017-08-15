651dc7360b96ba2218c95356cb35294a1b7d7378
Damian Legawiec <damian@sparksolutions.co>
2017-08-15 11:24:12 +0200

    Schema.db update

--- db/schema.rb
+++ db/schema.rb
@@ -207,7 +207,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["source_id", "source_type"], name: "index_spree_log_entries_on_source_id_and_source_type"
   end
 
-  create_table "spree_option_type_prototypes", id: :serial, force: :cascade do |t|
+  create_table "spree_option_type_prototypes", force: :cascade do |t|
     t.integer "prototype_id"
     t.integer "option_type_id"
     t.index ["option_type_id"], name: "index_spree_option_type_prototypes_on_option_type_id"
@@ -225,7 +225,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["position"], name: "index_spree_option_types_on_position"
   end
 
-  create_table "spree_option_value_variants", id: :serial, force: :cascade do |t|
+  create_table "spree_option_value_variants", force: :cascade do |t|
     t.integer "variant_id"
     t.integer "option_value_id"
     t.index ["option_value_id"], name: "index_spree_option_value_variants_on_option_value_id"
@@ -245,7 +245,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["position"], name: "index_spree_option_values_on_position"
   end
 
-  create_table "spree_order_promotions", id: :serial, force: :cascade do |t|
+  create_table "spree_order_promotions", force: :cascade do |t|
     t.integer "order_id"
     t.integer "promotion_id"
     t.index ["order_id"], name: "index_spree_order_promotions_on_order_id"
@@ -376,7 +376,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["product_id"], name: "index_spree_product_option_types_on_product_id"
   end
 
-  create_table "spree_product_promotion_rules", id: :serial, force: :cascade do |t|
+  create_table "spree_product_promotion_rules", force: :cascade do |t|
     t.integer "product_id"
     t.integer "promotion_rule_id"
     t.index ["product_id"], name: "index_products_promotion_rules_on_product_id"
@@ -419,7 +419,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["tax_category_id"], name: "index_spree_products_on_tax_category_id"
   end
 
-  create_table "spree_products_taxons", id: :serial, force: :cascade do |t|
+  create_table "spree_products_taxons", force: :cascade do |t|
     t.integer "product_id"
     t.integer "taxon_id"
     t.integer "position"
@@ -460,7 +460,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["taxon_id"], name: "index_spree_promotion_rule_taxons_on_taxon_id"
   end
 
-  create_table "spree_promotion_rule_users", id: :serial, force: :cascade do |t|
+  create_table "spree_promotion_rule_users", force: :cascade do |t|
     t.integer "user_id"
     t.integer "promotion_rule_id"
     t.index ["promotion_rule_id"], name: "index_promotion_rules_users_on_promotion_rule_id"
@@ -511,7 +511,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["name"], name: "index_spree_properties_on_name"
   end
 
-  create_table "spree_property_prototypes", id: :serial, force: :cascade do |t|
+  create_table "spree_property_prototypes", force: :cascade do |t|
     t.integer "prototype_id"
     t.integer "property_id"
     t.index ["property_id"], name: "index_spree_property_prototypes_on_property_id"
@@ -638,7 +638,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["return_authorization_id"], name: "index_spree_return_items_on_return_authorization_id"
   end
 
-  create_table "spree_role_users", id: :serial, force: :cascade do |t|
+  create_table "spree_role_users", force: :cascade do |t|
     t.integer "role_id"
     t.integer "user_id"
     t.index ["role_id"], name: "index_spree_role_users_on_role_id"
@@ -691,7 +691,7 @@ ActiveRecord::Schema.define(version: 20170804145180) do
     t.index ["shipping_method_id"], name: "index_spree_shipping_method_categories_on_shipping_method_id"
   end
 
-  create_table "spree_shipping_method_zones", id: :serial, force: :cascade do |t|
+  create_table "spree_shipping_method_zones", force: :cascade do |t|
     t.integer "shipping_method_id"
     t.integer "zone_id"
     t.index ["shipping_method_id"], name: "index_spree_shipping_method_zones_on_shipping_method_id"
