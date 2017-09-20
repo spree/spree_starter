27cd107a5d50f256e45835ca72f23a3975c8b037
Konrad Gorazd <konrad.gorazd@gmail.com>
2017-09-20 12:28:08 +0200

    Enable rollbar people tracking

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -11,9 +11,9 @@ Rollbar.configure do |config|
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
   # `username`, and `email` methods to fetch those properties. To customize:
-  # config.person_method = "my_current_user"
+  config.person_method = 'spree_current_user'
   # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
+  config.person_username_method = 'login'
   # config.person_email_method = "my_email"
 
   # If you want to attach custom data to all exception and message reports,
