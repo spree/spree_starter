53e48be3c05fcfa827dd2cc2723c9913682bb133
Punkbooster <ugliar.arsen@gmail.com>
2018-09-19 10:40:27 +0200

    Add webpacker initialz config & setup sample component

--- config/application.rb
+++ config/application.rb
@@ -41,17 +41,6 @@ module SparkStarterKit
     # adds support for Services
     config.autoload_paths += %W(#{config.root}/services #{config.root}/app/services/concerns)
 
-    # React-rails server side renderning
-    config.react.server_renderer_pool_size  ||= 1  # ExecJS doesn't allow more than one on MRI
-    config.react.server_renderer_timeout    ||= 20 # seconds
-    config.react.server_renderer = React::ServerRendering::SprocketsRenderer
-
-    config.react.server_renderer_options = {
-      code: 'var window = this; ',
-      files: ['application.server.js'], # files to load for prerendering
-      replay_console: true, # if true, console.* will be replayed client-side
-    }
-
     if Rails.env.production?
       # CloudFlare middleware for proper visitors IP addresses
       require "#{Rails.root}/lib/cloud_flare_middleware"
