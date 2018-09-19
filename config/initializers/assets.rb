c93097738645a3e67963f95ea1d8f5bb1a06105a
Punkbooster <ugliar.arsen@gmail.com>
2018-09-19 10:40:27 +0200

    Add webpacker initialz config & setup sample component

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,18 +3,5 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add folder with webpack generated assets to assets.paths
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'webpack')
-
-# webpack assets available for rails
-Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.jpg *.ico)
-Rails.application.config.assets.paths << Rails.root.join('client', 'img')
-Rails.application.config.assets.paths << Rails.root.join('client', 'fonts')
-Rails.application.config.assets.paths << Rails.root.join('client', 'css')
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(
-  application.*.*
-  server-bundle.js
-)
---
+++ config/webpack/custom.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, '../')
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      actions: path.join(process.cwd(), 'app/javascript/packs', 'actions'),
+      components: path.join(process.cwd(), 'app/javascript/packs', 'components'),
+      constants: path.join(process.cwd(), 'app/javascript/packs', 'constants'),
+      reducers: path.join(process.cwd(), 'app/javascript/packs', 'reducers'),
+      src: path.join(process.cwd(), 'app/javascript/packs', 'src')
+    }
+  }
+}
