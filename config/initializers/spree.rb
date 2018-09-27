47f955f977f14238608f9e0629a5ac6acfaec2fb
Damian Legawiec <damian@sparksolutions.co>
2018-09-27 18:52:23 +0200

    fix

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,7 +13,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-  config.layout = 'spree_application'
+  config.layout = 'spree/layouts/spree_application'
   config.logo = 'logo/spree_50.png'
   config.admin_interface_logo = 'admin/logo.png'
 end
