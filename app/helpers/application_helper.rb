8a3a4535de0ba3ead3fde2529a4773b1392718b8
Punkbooster <ugliar.arsen@gmail.com>
2018-10-01 15:22:37 +0200

    Remove unnecessary files & methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,1 @@
-module ApplicationHelper
-  def env_javascript_include_tag(prod_asset, dev_asset, params = {})
-    asset_file = !Rails.env.development? ? prod_asset : dev_asset
-    return javascript_include_tag(asset_file, params) if asset_file
-  end
-
-  def env_stylesheet_link_tag(prod_asset, dev_asset, params = {})
-    asset_file = !Rails.env.development? ? prod_asset : dev_asset
-    return stylesheet_link_tag(asset_file, params) if asset_file
-  end
-end
+module ApplicationHelper; end
