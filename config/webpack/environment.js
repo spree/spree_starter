d81ce9a4a7873003797173ecefba1aa6f3e52256
Damian Legawiec <damian@sparksolutions.co>
2019-02-06 15:19:20 +0100

    Updated webpacker setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,28 @@
 const { environment } = require('@rails/webpacker')
 
+const babelLoader = environment.loaders.get('babel')
+
+environment.loaders.insert(
+  'svg',
+  {
+    test: /\.svg$/,
+    use: babelLoader.use.concat([
+      {
+        loader: 'react-svg-loader',
+        options: {
+          jsx: true
+        }
+      }
+    ])
+  },
+  { after: 'file' }
+)
+
+const fileLoader = environment.loaders.get('file')
+fileLoader.exclude = /\.(svg)$/i
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+})
+
 module.exports = environment
