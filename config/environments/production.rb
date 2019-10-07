ff84222082e33ac4db2741f350a86ac86929c005
Damian Legawiec <damian@sparksolutions.co>
2019-10-07 16:34:02 +0200

    Added cache control for static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
