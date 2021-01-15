8043a602dba56036fc4a39a36f7959c537fb6a6c
Damian Legawiec <damian@sparksolutions.co>
2021-01-15 17:18:52 +0100

    Remove React

--- app/javascript/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import actionTypes from 'constants/index'
-
-export const nameSubmit = name => ({
-  type: actionTypes.CHANGE_NAME,
-  payload: name
-})
--- app/javascript/src/constants/index.js
+++
@@ -1,5 +0,0 @@
-import mirrorCreator from 'mirror-creator'
-
-const actionTypes = mirrorCreator(['CHANGE_NAME'])
-
-export default actionTypes
