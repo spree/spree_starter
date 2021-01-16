e610038ed5a33d4c2b71674416b35b27511b5a3e
Damian Legawiec <damian@sparksolutions.co>
2021-01-16 15:13:37 +0100

    fix spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'home page', js: true do
+RSpec.describe 'home page' do
   it 'returns 200 HTTP status' do
     visit '/'
     expect(page).to have_http_status('200')
