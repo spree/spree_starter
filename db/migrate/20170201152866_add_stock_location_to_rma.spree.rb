40a92daa36d52c751d5ff42d2fbe07978b7bf17d
Damian Legawiec <damian@sparksolutions.co>
2021-01-16 21:12:49 +0100

    Spree 4.1 upgrade - fix RMA class name

--- db/migrate/20170201152866_add_stock_location_to_rma.spree.rb
+++ db/migrate/20170201152866_add_stock_location_to_rma.spree.rb
@@ -1,5 +1,5 @@
 # This migration comes from spree (originally 20130326175857)
-class AddStockLocationToRma < ActiveRecord::Migration[4.2]
+class AddStockLocationToRMA < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_return_authorizations, :stock_location_id, :integer
   end
