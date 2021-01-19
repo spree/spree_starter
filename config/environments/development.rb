17dcc33b4e1c9dec8669b0ca3df387f35be5dda2
Damian Legawiec <damian@sparksolutions.co>
2021-01-19 19:20:06 +0100

    Allow to use lvh.me subdomains for multple stores in development
    Connected to https://github.com/spree/spree/pull/10695

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,4 +72,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  config.config.hosts << /.*\.lvh\.me/
 end
