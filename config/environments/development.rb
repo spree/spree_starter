da305b572178668361d258521e25b1ee4e575aba
Damian Legawiec <damian@sparksolutions.co>
2021-01-19 19:20:31 +0100

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  config.config.hosts << /.*\.lvh\.me/
+  config.hosts << /.*\.lvh\.me/
 end
