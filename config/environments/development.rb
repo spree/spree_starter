d4cac4bd42f103f3439a47a6cb91afebcb87aac0
Damian Legawiec <damian@sparksolutions.co>
2021-02-08 15:04:20 +0100

    Use Bullet only when defined

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,10 +64,12 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
+  if defined?(Bullet)
+    config.after_initialize do
+      Bullet.enable        = true
+      Bullet.console       = true
+      Bullet.rails_logger  = true
+    end
   end
 
   config.action_mailer.delivery_method = :letter_opener
