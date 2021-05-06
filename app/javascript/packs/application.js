c5904c956e13572a6f98731eecec033d99307cb5
Damian Kaczmarczyk <damia2kaczmarczyk@gmail.com>
2021-05-06 09:23:28 +0200

    Webpack clean install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-// Support component names which are placed under containers alias folder name
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+console.log('Hello World from Webpacker')
