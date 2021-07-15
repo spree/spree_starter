ae646ed3f056bcf6ab07160bba071bad0be81299
Damian Legawiec <damian@sparksolutions.co>
2021-07-15 15:48:55 +0200

    Use session across main domain and subdomains for multi store support

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_spark_starter_kit_session'
+Rails.application.config.session_store :cookie_store, key: '_spree_starter_session', domain: :all
