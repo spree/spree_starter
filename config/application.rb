a6b7cb9bdd8e8dd71c31975a574be03426b74b2b
Damian Legawiec <damian@sparksolutions.co>
2021-08-03 12:09:11 +0200

    Remove forced available locales

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,6 @@ module SpreeStarter
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
-    I18n.available_locales = :en
 
     # use Sidekiq for ActiveJob
     config.active_job.queue_adapter = :sidekiq
