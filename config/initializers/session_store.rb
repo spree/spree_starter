ddcf3e3e5cf23ba3d8c6bc1daee2831942cbaf17
Damian Legawiec <damian@sparksolutions.co>
2021-08-17 23:42:39 +0200

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_spree_starter_session', domain: :all
+cookie_domain = ENV['COOKIE_DOMAIN'] == 'all' ? :all : ENV['COOKIE_DOMAIN']
+
+Rails.application.config.session_store :cookie_store, key: '_spree_starter_session', domain: cookie_domain
