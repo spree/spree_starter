2914ff75da9fbd96ea94ba3990d21787efa86b73
Damian Legawiec <damian@sparksolutions.co>
2021-09-09 18:27:20 +0200

    Setup seed data and add button

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+bundle exec rails db:seed
+bundle exec rake spree_sample:load
