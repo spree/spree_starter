611d3c487415d43d022bf72ff144d708a098e3ce
Damian Legawiec <damian@sparksolutions.co>
2021-09-13 12:25:33 +0200

    Allow all hosts for dev environment (#974)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  config.hosts << /.*\.lvh\.me/
+  config.hosts.clear
 end
