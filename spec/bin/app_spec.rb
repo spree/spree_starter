f79ceedb84af59e2fbb1cf51585b8309886e42e0
Rafal Kosla <rafalkosla101@gmail.com>
2022-10-07 09:22:52 +0200

    feat: add local server spec

---
+++ spec/bin/app_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "app" do
+  context "run local server" do
+    before do
+      fork { system('bin/setup-no-docker') }
+    end
+    it "starts local services" do
+      wait_for_server
+      expect(Net::HTTP.get_response(URI("http://127.0.0.1:4000/admin")).code).to eq('200')
+      expect(Net::HTTP.get_response(URI("http://127.0.0.1:4000/api/v2/storefront/taxons")).code).to eq('200')
+    end
+  end
+end
