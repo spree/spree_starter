5c6d517b66147854e6e3c36aa665184a9d37c110
nciemniak <nciemniak@gmail.com>
2022-11-23 11:52:48 +0100

    change order of build script cmds

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 yes | bundle exec rails javascript:install:esbuild
 yes | bundle exec rails turbo:install
 yes | bundle exec rails g spree:frontend:install
 yarn build
+bundle exec rails assets:precompile
