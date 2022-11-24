8c234aaaae0fef9360fa457e9eca8a864a8cae13
nciemniak <nciemniak@gmail.com>
2022-11-24 14:01:38 +0100

    Add notes to render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,12 @@ set -o errexit
 bundle install
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
+# Setup commands specific to legacy frontend
 yes | bundle exec rails javascript:install:esbuild
 yes | bundle exec rails turbo:install
 yes | bundle exec rails g spree:frontend:install
 yarn build
+
+# asset precompile must be run after installing turbo, etc.
 bundle exec rails assets:precompile
