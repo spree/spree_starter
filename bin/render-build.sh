1392e015bb082363ca7472b6490f8f694af09521
nciemniak <nciemniak@gmail.com>
2022-11-25 09:36:49 +0100

    Revert "add option to deploy to render w legacy frontend"

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# Note: any build commands added/changed in this script should also be update to render-build-legacy-frontend.sh
-
 # exit on error
 set -o errexit
 
@@ -8,3 +6,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+bundle exec rails db:seed
+bundle exec rake spree_sample:load
