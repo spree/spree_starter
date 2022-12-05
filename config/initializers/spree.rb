21ea38d0afca3eaa151dcc4e536f6b081712079c
nciemniak <nciemniak@gmail.com>
2022-12-05 11:43:22 +0100

    Update spree config initialization to be compatible w rails 7

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,10 +9,12 @@
 #
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
-Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+Rails.application.config.after_initialize do
+  Spree.config do |config|
+    # Example:
+    # Uncomment to stop tracking inventory levels in the application
+    # config.track_inventory_levels = false
+  end
 end
 
 Spree.user_class = 'Spree::User'
