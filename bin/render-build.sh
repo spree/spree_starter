fad185a0884b5ae771e4436ac3a16dabd2d805c5
nciemniak <nciemniak@gmail.com>
2022-12-09 13:35:20 +0100

    include db seed and updates render service names

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+bundle exec rails db:seed
+bundle exec rake spree_sample:load
 
 # Setup commands specific to legacy frontend
 yes | bundle exec rails javascript:install:esbuild
