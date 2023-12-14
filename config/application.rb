24aab1f47178cabb34a8bd63fa257520663f152a
Mateusz Porębski <mateusz.porebski99@gmail.com>
2023-12-14 13:15:31 +0100

    zeitwerk conditionally ignore payments_controller

--- config/application.rb
+++ config/application.rb
@@ -48,5 +48,14 @@ module SpreeStarter
       require "#{Rails.root}/lib/cloud_flare_middleware"
       config.middleware.insert_before(0, Rack::CloudFlareMiddleware)
     end
+
+    # ignoring files by Zeitwerk when spree_frontend isn't loaded
+    unless ::Spree::Core::Engine.frontend_available?
+      Dir.chdir(Rails.root.join(".."))
+      files_to_ignore = Dir["**/spree_stripe/payments_controller.rb"]
+      files_to_ignore.each { |file| 
+        Rails.autoloaders.main.ignore(Rails.root.join("../#{file}"))
+      }
+    end
   end
 end
