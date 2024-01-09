661466f6a84bb431a06da7dd415cb79f004b1a70
Rafal Cymerys <rafal@upsidelab.io>
2024-01-09 14:03:11 +0100

    Exclude development and test from build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@
 # exit on error
 set -o errexit
 
-bundle install
+bundle install --without development test
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
