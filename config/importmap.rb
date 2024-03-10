1fbc17bc8ff5d0145d358be706638032395c15c6
Damian Legawiec <damian@getvendo.com>
2024-03-10 18:32:46 +0100

    bump spree backend

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "sortablejs" # @1.15.2
-pin_all_from Spree::Backend::Engine.root.join("app/javascript/controllers"), under: "controllers", to: "controllers"
-pin "request_utility", to: "request_utility"
