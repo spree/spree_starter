a9f81f311fa8249d96048138b4b94bc7bf7a1adb
Damian Legawiec <damian@getvendo.com>
2024-03-10 17:20:46 +0100

    fix request utility import

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sortablejs" # @1.15.2
 pin_all_from Spree::Backend::Engine.root.join("app/javascript/controllers"), under: "controllers", to: "controllers"
+pin "request_utility", to: "request_utility"
