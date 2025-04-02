3909ab943ba6b33b2c5ab61a1fecc775d9a888de
Damian Legawiec <damian@getvendo.com>
2025-04-02 13:23:42 +0200

    Drop redis in CI, use test active job adapter, fix CircleCI config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Use test active job adapter
+  config.active_job.queue_adapter = :test
 end
