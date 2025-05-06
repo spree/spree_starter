2a0a4023a7f434ea186514425c2eec7bfb4d21db
Damian Kaczmarczyk <git@vegann.space>
2025-05-06 17:06:12 +0200

    Update config/environments/production.rb
    Co-authored-by: Damian Legawiec <damian@getvendo.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     ActionMailer::Base.smtp_settings = {
       user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
       password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-      domain: ENV['APP_DOMAIN'],
+      domain: Rails.application.routes.default_url_options[:host]
       address: 'smtp.sendgrid.net',
       port: 587,
       authentication: :plain,
