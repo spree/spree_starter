f1381446ba1db7987347bd4bd63a506c64f476fa
Dina Kazakevich <29330744+dinakazakevich@users.noreply.github.com>
2025-05-26 17:15:37 +0400

    Testing a hypothesis with some dummy data

--- playwright/tests/mockedPaymentCheckout.na.spec.ts
+++ playwright/tests/mockedPaymentCheckout.na.spec.ts
@@ -12,12 +12,22 @@ test('checkout flow with mocked payment confirmation', async ({
   checkoutPage,
 }) => {
   // Mock the api call for the final step of the checkout flow
-  await checkoutPage.page.route('*/**/checkout/**/update/payment', async (route, request) => {
-    if (request.method() === 'POST') {
+  await checkoutPage.page.route('**/products/denim-shirt', async (route, request) => {
+    const response = await route.fetch();
+    const body = await response.text();
+    const modified = body.replace(/Denim Shirt/g, 'Strawberry Shirt!');
+
+    if (request.method() === 'GET') {
       await route.fulfill({
-        status: 200,
-        contentType: 'text/html',
-        body: successResponse,
+        // status: 200,
+        // contentType: 'text/html',
+        status: response.status(),
+        // body: successResponse,
+        body: modified,
+        headers: {
+          ...response.headers(),
+          'Content-Type': 'text/html',
+        },
       });
     } else {
       await route.continue();
@@ -27,35 +37,36 @@ test('checkout flow with mocked payment confirmation', async ({
   await homePage.page.goto('/products');
 
   // Select a random product
-  await productsPage.selectProduct();
+  await productsPage.selectProduct('Denim Shirt');
 
-  // Select size 'M'
-  await productDetailsPage.selectSize('M');
+  await expect(productDetailsPage.page.getByRole('heading', { name: 'Strawberry Shirt' })).toBeVisible();
+  // // Select size 'M'
+  // await productDetailsPage.selectSize('M');
 
-  // Add the product to cart
-  await productDetailsPage.addToCart();
+  // // Add the product to cart
+  // await productDetailsPage.addToCart();
 
-  // Check that cart total is greater than 0
-  const cartTotalValue = await productDetailsPage.cartSidebar.getCartTotal();
-  expect(cartTotalValue).toBeGreaterThan(0);
+  // // Check that cart total is greater than 0
+  // const cartTotalValue = await productDetailsPage.cartSidebar.getCartTotal();
+  // expect(cartTotalValue).toBeGreaterThan(0);
 
-  // Proceed to checkout
-  await productDetailsPage.cartSidebar.proceedToCheckout();
+  // // Proceed to checkout
+  // await productDetailsPage.cartSidebar.proceedToCheckout();
 
-  // Fill out customer email
-  await checkoutPage.fillEmail(faker.internet.email());
+  // // Fill out customer email
+  // await checkoutPage.fillEmail(faker.internet.email());
 
-  // Submit shipping address
-  await checkoutPage.fillShippingAddress();
-  await checkoutPage.page.getByRole('button', { name: 'Save and Continue' }).click();
+  // // Submit shipping address
+  // await checkoutPage.fillShippingAddress();
+  // await checkoutPage.page.getByRole('button', { name: 'Save and Continue' }).click();
 
-  // Accept the default delivery option
-  await checkoutPage.acceptDefaultShipping();
+  // // Accept the default delivery option
+  // await checkoutPage.acceptDefaultShipping();
 
-  // Submit in payment details
-  await checkoutPage.fillPaymentDetail();
-  await checkoutPage.page.getByRole('button', { name: 'Pay' }).click();
+  // // Submit in payment details
+  // await checkoutPage.fillPaymentDetail();
+  // await checkoutPage.page.getByRole('button', { name: 'Pay' }).click();
 
-  // Assert that the mocked order confirmation page with 'Strawberry' client name loaded
-  await expect(homePage.page.getByText('Thanks Strawberry for your order!')).toBeVisible();
+  // // Assert that the mocked order confirmation page with 'Strawberry' client name loaded
+  // await expect(homePage.page.getByText('Thanks Strawberry for your order!')).toBeVisible();
 });
