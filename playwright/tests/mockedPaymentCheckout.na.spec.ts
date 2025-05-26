aec7aaea3133a74333d55ccf3e6cbb7a3f68b7f7
Dina Kazakevich <29330744+dinakazakevich@users.noreply.github.com>
2025-05-26 16:42:28 +0400

    Returned to the original solution for mocking

--- playwright/tests/mockedPaymentCheckout.na.spec.ts
+++ playwright/tests/mockedPaymentCheckout.na.spec.ts
@@ -1,17 +1,17 @@
-import { test, expect } from '../lib/fixtures/authenticate';
+import { test, expect } from '../lib/fixtures/instantiate';
 import { successResponse } from '../lib/datafactory/mockCheckoutConfirmation';
 import { generateUser } from '../lib/datafactory/testData';
+import { faker } from '@faker-js/faker';
 
 const testUser = generateUser();
 test.use({ userParams: testUser });
 test('checkout flow with mocked payment confirmation', async ({
+  homePage,
   productsPage,
   productDetailsPage,
   checkoutPage,
-  authenticatedHomePage,
 }) => {
   // Mock the api call for the final step of the checkout flow
-
   await checkoutPage.page.route('*/**/checkout/**/update/payment', async (route, request) => {
     if (request.method() === 'POST') {
       await route.fulfill({
@@ -24,7 +24,7 @@ test('checkout flow with mocked payment confirmation', async ({
     }
   });
 
-  await authenticatedHomePage.page.goto('/products');
+  await homePage.page.goto('/products');
 
   // Select a random product
   await productsPage.selectProduct();
@@ -43,7 +43,7 @@ test('checkout flow with mocked payment confirmation', async ({
   await productDetailsPage.cartSidebar.proceedToCheckout();
 
   // Fill out customer email
-  //   await checkoutPage.fillEmail(faker.internet.email());
+  await checkoutPage.fillEmail(faker.internet.email());
 
   // Submit shipping address
   await checkoutPage.fillShippingAddress();
@@ -56,8 +56,6 @@ test('checkout flow with mocked payment confirmation', async ({
   await checkoutPage.fillPaymentDetail();
   await checkoutPage.page.getByRole('button', { name: 'Pay' }).click();
 
-  await checkoutPage.page.waitForURL('*/**/checkout/**/update/payment');
-
   // Assert that the mocked order confirmation page with 'Strawberry' client name loaded
-  await expect(checkoutPage.page.getByText('Thanks Strawberry for your order!')).toBeVisible();
+  await expect(homePage.page.getByText('Thanks Strawberry for your order!')).toBeVisible();
 });
