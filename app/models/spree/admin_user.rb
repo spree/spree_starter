3c984ed05735aa379d2bcc0c38c9cb711134f2b2
Damian Legawiec <damian@getvendo.com>
2025-07-17 16:52:19 +0200

    Update admin_user.rb

--- app/models/spree/admin_user.rb
+++ app/models/spree/admin_user.rb
@@ -2,7 +2,7 @@ class Spree::AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   include Spree::UserMethods
 end
