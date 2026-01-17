b32e7e8792688bce8058e90053033077d96638bb
Damian Legawiec <damian@getvendo.com>
2026-01-17 10:42:16 +0100

    Bump Rails to 8.1 and use Spree 5.2.6

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module SpreeStarter
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 8.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
